e2f462a1e7b2ba1e5c06c73a3692aca16fe2c6f8
ikonst87 <ikonstantinovskyi@bostonsd.ru>
2021-07-15 18:36:57 +0300

    Added data Created base template Created templates for each page Updated route logic Update Jinja templates logic

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 from flask import Flask, render_template
 import data
 
+tours = data.tours
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('index.html', departures=data.departures, title=data.title, tours=tours)
 
 
 @app.route("/departures/<departure>/")
 def render_departure(departure):
-    return render_template('departure.html')
+    try:
+        dep_city = data.departures[departure].replace("Из", "из")
+    except KeyError:
+        return "Нет такого направления", 404
+    tours_dep = {}
+    for tour_id, tour_data in tours.items():
+        if tour_data["departure"] == departure:
+            tours_dep[tour_id] = tour_data
+
+    return render_template('departure.html', departures=data.departures, title=data.title, tours=tours_dep,
+                           dep_city=dep_city)
 
 
 @app.route("/tours/<id>/")
 def render_tours(id):
-    return render_template('tour.html')
+    tour_id = int(id)
+    try:
+        tour = tours[tour_id]
+        dep_city = data.departures[tour["departure"]].replace("Из", "из")
+    except KeyError:
+        return "Такого тура у нас нет", 404
+    return render_template('tour.html', departures=data.departures, title=data.title, tour=tour, dep_city=dep_city)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)
